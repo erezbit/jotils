7df501a94706d17e1c41a02f36cf3003afce4d82
erezbit <43997110+erezbit@users.noreply.github.com>
2018-11-01 16:06:34 +0200

    Update index.js

--- src/components/random/index.js
+++ src/components/random/index.js
@@ -1,7 +1,7 @@
 /**
  * @description
- * random number between 0 and 1, or between 0 and max parameter(optional)
- * @param {number} max max parameter(optional)
+ * random number between 0 and 1 (decimal), or between 0 and max parameter (optional)
+ * @param {number} max max parameter (optional)
  * @returns {number} random number
  * @example
  * random() // => 0.123
